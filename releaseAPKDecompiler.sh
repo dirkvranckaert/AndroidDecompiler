28ee83c5520a14309fbefd91e74a28cb6c6cd817
Dirk Vranckaert <dirkvranckaert@gmail.com>
2014-10-02 10:49:53 +0200

    Added terminal colors ;-)

--- releaseAPKDecompiler.sh
+++ releaseAPKDecompiler.sh
@@ -45,29 +45,31 @@ then
   exit
 fi
 
-echo "Did you update the help option with the new features being released (N)?"
+echo "\x1B[01;91mDid you update the help option with the new features being released (N)?"
 read questionHelpOptionUpdated
 if [ "$questionHelpOptionUpdated" != "Y" ] && [ "$questionHelpOptionUpdated" != "y" ]
 then
-	echo "Update and commit (and push) the changes before trying again"
+	echo "\x1B[01;93mUpdate and commit (and push) the changes before trying again\x1B[0m"
 	exit;
 fi
 
-echo "Did you update the README with everything that is important for this new release (N)?"
+echo "\x1B[01;91mDid you update the README with everything that is important for this new release (N)?"
 read questionReadmeUpdated
 if [ "$questionReadmeUpdated" != "Y" ] && [ "$questionReadmeUpdated" != "y" ]
 then
-	echo "Update and commit (and push) the changes before trying again"
+	echo "\x1B[01;93mUpdate and commit (and push) the changes before trying again\x1B[0m"
 	exit;
 fi
 
-echo "Did you commit and push any other changes (eg: release script) (N)?"
+echo "\x1B[01;91m Did you commit and push any other changes (eg: release script) (N)?"
 read questionEverythingPushed
 if [ "$questionEverythingPushed" != "Y" ] && [ "$questionEverythingPushed" != "y" ]
 then
-	echo "Commit and push all your changes before trying again"
+	echo "\x1B[01;93mCommit and push all your changes before trying again\x1B[0m"
 	exit;
 fi
+
+echo " \x1B[0m"
 
 echo "Releasing version $version to directory $outputDir"
 echo ""
