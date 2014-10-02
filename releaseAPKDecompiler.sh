e1b302ac4919609c666d0cfe01444963107f2c1d
Dirk Vranckaert <dirkvranckaert@gmail.com>
2014-10-02 10:40:46 +0200

    Added some security questions to be sure to not forget anything before releasing

--- releaseAPKDecompiler.sh
+++ releaseAPKDecompiler.sh
@@ -45,6 +45,30 @@ then
   exit
 fi
 
+echo "Did you update the help option with the new features being released (N)?"
+read questionHelpOptionUpdated
+if [ "$questionHelpOptionUpdated" != "Y" ] && [ "$questionHelpOptionUpdated" != "y" ]
+then
+	echo "Update and commit (and push) the changes before trying again"
+	exit;
+fi
+
+echo "Did you update the README with everything that is important for this new release (N)?"
+read questionReadmeUpdated
+if [ "$questionReadmeUpdated" != "Y" ] && [ "$questionReadmeUpdated" != "y" ]
+then
+	echo "Update and commit (and push) the changes before trying again"
+	exit;
+fi
+
+echo "Did you commit and push any other changes (eg: release script) (N)?"
+read questionEverythingPushed
+if [ "$questionEverythingPushed" != "Y" ] && [ "$questionEverythingPushed" != "y" ]
+then
+	echo "Commit and push all your changes before trying again"
+	exit;
+fi
+
 echo "Releasing version $version to directory $outputDir"
 echo ""
 
